f7dbb3721b1918580fc3ab8b2a3d2d9cd7fa7f90
Uttam2021 <uttamshukla12345@gmail.com>
2023-10-13 10:44:50 +0530

    one to one mapping

---
+++ src/main/java/com/crud/mappings/CrudMappingsApplication.java
@@ -0,0 +1,13 @@
+package com.crud.mappings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMappingsApplication.class, args);
+	}
+
+}
